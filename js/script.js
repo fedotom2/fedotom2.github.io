30e421c5d9b828c3f31900f29c301ae5a258cc6b
max <fedotom2@gmail.com>
2016-06-09 01:02:08 +0300

    margarita/menu

--- js/script.js
+++ js/script.js
@@ -20,11 +20,13 @@
 })();
 
 $(document).ready(function(){
-    $('area[href^="#"]').click(function(){
+
+    $('a[href^="#"]').click(function(){
         var el = $(this).attr('href');
         $('body').animate({
             scrollTop: $(el).offset().top
         }, 2000);
         return false;
     });
+    
 });
