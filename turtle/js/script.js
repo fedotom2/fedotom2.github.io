24d077a738e450ec86ac8f9f3102c55c1505794f
Boris <fedotom2@gmail.com>
2020-03-18 08:45:34 +0200

    Turtle

---
+++ turtle/js/script.js
@@ -0,0 +1,175 @@
+'use strict';
+
+const size = {
+  w: 800,
+  h: 600
+};
+
+canvas.width = size.w;
+canvas.height = size.h;
+
+const ctx = canvas.getContext('2d');
+
+const Turtle = function (x, y) {
+  this.x = x;
+  this.y = y;
+  this.radius = 30;
+  this.angle = 0;
+};
+
+Turtle.prototype.draw = function (ctx, size) {
+  const img = new Image();
+  const that = this;
+  img.addEventListener('load', function() {
+    ctx.save(); 
+    ctx.translate(that.x, that.y);
+    ctx.rotate(that.angle);
+    ctx.drawImage(img, 0, 0, that.radius, that.radius);
+    ctx.restore(); 
+  }, false);
+
+  img.src = './images/turtle.png';
+};
+
+Turtle.prototype.update = function (ctx, size) {
+
+};
+
+let algo = [], vectors = [];
+let interval;
+const turtle = new Turtle(size.w / 2, size.h / 2);
+
+const front = (ctx, step, turtle) => {
+  const x = turtle.x;
+  const y = turtle.y;
+  const vector = [];
+  return () => {
+    ctx.beginPath();
+    ctx.moveTo(turtle.x + turtle.radius / 2, turtle.y + turtle.radius / 2);
+    vector.push(turtle.x);
+    vector.push(turtle.y);
+    turtle.x += step * Math.sin(turtle.angle);
+    turtle.y -= step * Math.cos(turtle.angle);
+    ctx.lineTo(turtle.x + turtle.radius / 2, turtle.y + turtle.radius / 2);
+    vector.push(turtle.x);
+    vector.push(turtle.y);
+    ctx.closePath();
+    ctx.stroke();
+    vectors.push(vector);
+  };
+};
+
+const back = (ctx, step, turtle) => {
+  return () => {
+    ctx.beginPath();
+    ctx.moveTo(turtle.x + turtle.radius / 2, turtle.y - turtle.radius / 2);
+    turtle.x -= step * Math.sin(turtle.angle);
+    turtle.y += step * Math.cos(turtle.angle);
+    ctx.lineTo(turtle.x + turtle.radius / 2, turtle.y - turtle.radius / 2);
+    ctx.closePath();
+    ctx.stroke();
+  };
+};
+
+const right = (ctx, angle, turtle) => {
+  return () => {
+    turtle.angle += angle * Math.PI / 180;
+  };
+};
+
+const left = (ctx, angle, turtle) => {
+  return () => {
+    turtle.angle -= angle * Math.PI / 180;
+  };
+};
+
+const draw = (ctx, size) => {
+  turtle.draw(ctx, size);
+  for (let vector of vectors) {
+    ctx.moveTo(vector[0], vector[1]);
+    ctx.lineTo(vector[2], vector[3]);
+  }
+};
+
+const update = (ctx, size) => {
+  turtle.update(ctx, size);
+};
+
+const loop = () => {
+  // ctx.clearRect(turtle.x, turtle.y, turtle.radius, turtle.radius);
+  draw(ctx, size);
+  update(ctx, size);
+
+  requestAnimationFrame(loop);
+};
+
+loop();
+
+const frontBtnHandler = () => {
+  const step = +prompt('Введіть кількість кроків');
+  algo.push(front(ctx, step, turtle));
+  display.innerHTML += 'Вперед на ' + step + ' кроків<br>';
+};
+
+const backBtnHandler = () => {
+  const step = +prompt('Введіть кількість кроків');
+  algo.push(back(ctx, step, turtle));
+  display.innerHTML += 'Назад на ' + step + ' кроків<br>';
+};
+
+const rightBtnHandler = () => {
+  const angle = +prompt('Введіть кут повороту');
+  algo.push(right(ctx, angle, turtle));
+  display.innerHTML += 'Вправо на ' + angle + ' градусів<br>';
+};
+
+const leftBtnHandler = () => {
+  const angle = +prompt('Введіть кут повороту');
+  algo.push(left(ctx, angle, turtle));
+  display.innerHTML += 'Вліво на ' + angle + ' градусів<br>';
+};
+
+const repeatStartBtnHadnler = () => {
+
+};
+
+const repeatEndBtnHandler = () => {
+
+};
+
+let i = 0;
+const enterBtnHandler = () => {
+  // if (!interval) {
+  //   interval = setInterval(() => {
+  //     if (i < algo.length)
+  //       algo[i++]();
+  //     else {
+  //       clearInterval(interval);
+  //       interval = null;
+  //     }
+  //   }, 100);
+  // } else {
+  //   clearInterval(interval);
+  //   interval = null;
+  // }
+  for (let f of algo)
+    f();
+};
+
+const clearBtnHandler = () => {
+  ctx.clearRect(0, 0, size.w, size.h)
+  algo = [];
+  display.innerHTML = '';
+  turtle.x = size.w / 2;
+  turtle.y = size.h / 2;
+  turtle.angle = 0;
+};
+
+frontBtn.addEventListener('click', frontBtnHandler);
+backBtn.addEventListener('click', backBtnHandler);
+rightBtn.addEventListener('click', rightBtnHandler);
+leftBtn.addEventListener('click', leftBtnHandler);
+repeatStartBtn.addEventListener('click', repeatStartBtnHadnler);
+repeatEndBtn.addEventListener('click', repeatEndBtnHandler);
+enterBtn.addEventListener('click', enterBtnHandler);
+clearBtn.addEventListener('click', clearBtnHandler);
